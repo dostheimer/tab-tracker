5d780bd9ced87f7fd50dce8013ba34e0968d9b61
Drew Ostheimer <drewo@YULL10271.local>
2020-03-02 22:30:45 -0500

    Removed un-needed package.

--- client/src/main.js
+++ client/src/main.js
@@ -4,13 +4,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import { sync } from 'vuex-router-sync'
 import Panel from '@/components/Panel'
 
 Vue.config.productionTip = false
-
-// const unsync = sync(store, router)
-sync(store, router)
 
 Vue.component('panel', Panel)
 
