5363e387257d9006ebe4fb26e3db164690dc2ec5
Drew Ostheimer <drew.ostheimer@cae.com>
2020-05-05 16:49:35 -0400

    Fixed security vulnerabilities

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -3,7 +3,7 @@ import store from '../store/index'
 
 export default () => {
   return axios.create({
-    baseURL: 'http://localhost:8081',
+    baseURL: 'http://localhost:8081/api',
     headers: {
       Authorization: `Bearer ${store.state.token}`
     }
